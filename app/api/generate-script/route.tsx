23bc7250b4b190753364eedc8e63b013b9e09fc6
ManishxNinja <manishkaushik2243@gmail.com>
2025-05-30 14:34:22 +0530

    Added the video Script from the APi from the OPen ai

---
+++ app/api/generate-script/route.tsx
@@ -0,0 +1,37 @@
+import { GENERATE_SCRIPT_PROMPT } from "@/services/Prompt";
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+
+export const openai = new OpenAI({
+
+    baseURL: "https://openrouter.ai/api/v1",
+
+    apiKey: process.env.OPENROUTER_API_KEY,
+
+  });
+
+export async function POST(req: NextRequest) {
+  try {
+    const { topic } = await req.json();
+    if (!topic) {
+      return NextResponse.json({ error: "Topic is required" }, { status: 400 });
+    }
+
+    const prompt = GENERATE_SCRIPT_PROMPT.replace('{topic}', topic);
+
+    const completion = await openai.chat.completions.create({
+      model: "openai/gpt-3.5-turbo",
+
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    return NextResponse.json({ content: completion.choices[0].message?.content });
+  } catch (error) {
+    console.error("API Route Error:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error", details: error instanceof Error ? error.message : error },
+      { status: 500 }
+    );
+  }
+}
--- app/workspace/create-ad/page.tsx
+++ app/workspace/create-ad/page.tsx
@@ -2,12 +2,28 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Sparkles } from 'lucide-react'
+import { LoaderCircle, Sparkles } from 'lucide-react'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import axios from "axios"
 
 function createAd() {
   const[userInput, setUserInput] = useState<string | undefined>();
+  const[loading,setLoading] = useState(false);
+  const GenerateAIVideoScript=async()=>{
+    setLoading(true);
+    try {
+      console.log("Request is sended");
+      const result = await axios.post('/api/generate-script',{
+        topic: userInput
+      });
+      console.log(result.data);
+    } catch(err) {
+      console.log("some error occured",err);
+    }
+    
+    setLoading(false);
+  }
   return (
     <div className='p-10 mt-32 flex flex-col items-center justify-center'>
       <div>
@@ -16,10 +32,10 @@ function createAd() {
       <h2 className='font-bold text-2xl text-center'>🎥 Create AI Video Ads in Just One Click!</h2>
       <p className='mt-2 text-lg text-gray-500'>🚀 Turn your ideas into stunning, scroll-stopping videos — instantly, effortlessly, and without editing skills!</p>
       <Input onChange={(e) => setUserInput(e.target.value)} placeholder='Enter the topic or product info' className={'w-[500px] text-lg mt-5 p-5'} />
-      <Button className='mt-2 w-[250px] font-semibold'><Sparkles />Generate</Button>
+      <Button disabled={loading} onClick={GenerateAIVideoScript} className='mt-2 w-[250px] font-semibold'>{loading? <LoaderCircle className='animate-spin' />:<Sparkles />}Generate</Button>
     </div>
   )
-}
+} 
 
 export default createAd
 
